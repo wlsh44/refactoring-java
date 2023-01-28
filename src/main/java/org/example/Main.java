de20c0e71c4f1e2e29dcd187b979773b8048e7b0
wlsh44 <k87913j@gmail.com>
2023-01-28 18:04:13 +0900

    refactor: amoutFor 메서드 추출

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,24 +19,7 @@ public class Main {
 
         for (var perf : invoice.getPerformances()) {
             PlayInfo play = plays.get(perf.getPlayID());
-            int thisAmount = 0;
-
-            switch (play.getType()) {
-                case "tragedy" -> { //비극
-                    thisAmount = 40000;
-                    if (perf.getAudience() > 30) {
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                }
-                case "comedy" -> { //희극
-                    thisAmount = 30000;
-                    if (perf.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                }
-                default -> throw new IllegalArgumentException(String.format("알 수 없는 장르: %s", play.getType()));
-            }
+            int thisAmount = amountFor(perf, play);
 
             //포인트를 접립한다.
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
@@ -52,5 +35,27 @@ public class Main {
         result.append(String.format("총액: %s\n", format.format(totalAmount / 100.)));
         result.append(String.format("적립 포인트: %s점\n", volumeCredits));
         return result.toString();
+    }
+
+    private int amountFor(Performance aPerformance, PlayInfo play) {
+        int result = 0;
+
+        switch (play.getType()) {
+            case "tragedy" -> { //비극
+                result = 40000;
+                if (aPerformance.getAudience() > 30) {
+                    result += 1000 * (aPerformance.getAudience() - 30);
+                }
+            }
+            case "comedy" -> { //희극
+                result = 30000;
+                if (aPerformance.getAudience() > 20) {
+                    result += 10000 + 500 * (aPerformance.getAudience() - 20);
+                }
+                result += 300 * aPerformance.getAudience();
+            }
+            default -> throw new IllegalArgumentException(String.format("알 수 없는 장르: %s", play.getType()));
+        }
+        return result;
     }
 }
